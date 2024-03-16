4b45b9668fbc5a9e0622a38bdb81c50b90996694
Nikifor Bogdanov <nikiforn04@gmail.com>
2024-03-16 13:18:56 +0200

    add poll

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,9 +1,10 @@
 'use client'
-import { Button, Grid, IconButton, Input, InputAdornment, Stack, Typography } from "@mui/material";
+import { Box, Button, FormControlLabel, Grid, IconButton, Input, InputAdornment, Modal, Radio, RadioGroup, Stack, Typography } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
 import { darkBlue, lightBlue, orange, red, white } from "../Theme/theme";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
+import { grey } from "@mui/material/colors";
 
 
 const BASE_URL = "http://localhost:4000";
@@ -12,59 +13,128 @@ const socket = io(BASE_URL, {
   autoConnect: false,
 });
 
+enum MessageType {
+    USER="user",
+    ANNOUNCEMENT="announcement",
+}
+
 interface IMessage {
   text: string;
   user: number;
+  type: MessageType;
+}
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+}
+
+enum PollValue {
+    CHANGE_TOPIC="change topic",
+    KEEP_TOPIC="keep topic",
+}
+
+const pollOptions = [
+    {value: PollValue.CHANGE_TOPIC, label: "Change Topic"},
+    {value: PollValue.KEEP_TOPIC, label: "Keep Topic"},
+]
+
+const getMessageStyle = (props: any) => {
+    if (props.message.type === MessageType.ANNOUNCEMENT) {
+        return {
+            background: "radial-gradient(circle, rgba(255,255,255,1) 9%, rgba(48,48,48,1) 200%)|",
+            borderRadius: "16px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(11.9px)",
+            WebkitBackdropFilter: "blur(11.9px)",
+            border: "1px solid rgba(0, 0, 0, 0.2)",
+            padding: "8px 16px",
+            margin: "8px",
+        }
+    }
+    if (props.message.type === MessageType.USER) {
+        return {
+            background: props.message.user === props.user ? darkBlue : lightBlue,
+            borderRadius: "8px",
+            margin: "8px",
+            maxWidth: "70%",
+            border: "1px solid #fsfsfs",
+            boxShadow: "0px 4px 3px #5f5f5f",
+            padding: "8px 16px",
+            animationName: `${props.message.user === props.user ? "fadeFromRight" : "fadeFromLeft"}`,
+            animationDuration: "1s",
+        }
+    }
+}
+
+const getTextStyle = (props: any) => {
+    if (props.message.type === MessageType.ANNOUNCEMENT) {
+        return {
+            color: "#000000",
+            fontSize: "20px",
+        }
+    }
+    if (props.message.user === props.user) {
+        return {
+            color: white,
+            fontSize: "20px",
+        }
+    }
+    return {
+        color: "#000000",
+        fontSize: "20px",
+    }
+
+}
+
+const getJustify = (props: any) => {
+    if (props.message.type === MessageType.ANNOUNCEMENT) {
+        return "center";
+    } else if (props.message.user === props.user) {
+        return "end";
+    } else {
+        return "start";
+    }
+
 }
 
 const MessageBox = (props: {message: IMessage, user: number}) => {
-  return (
-   <Grid container width={"100%"} justifyContent={props.message.user === props.user ? "end" : "start"}>
-      <Grid sx={{
-        background: props.message.user === props.user ? darkBlue : lightBlue,
-        borderRadius: "8px",
-        margin: "8px",
-        maxWidth: "70%",
-        border: "1px solid #fsfsfs",
-        boxShadow: "0px 4px 3px #5f5f5f",
-        padding: "8px 16px"
-      }}>
-        <Typography sx={{
-          color: props.message.user === props.user ? white : "#000000",
-          fontSize: "20px",
-        }}>{props.message.text}</Typography>
-      </Grid>
-   </Grid>
-  )
+    return (
+    <Grid container width={"100%"} justifyContent={getJustify(props)}>
+        <Grid sx={getMessageStyle(props)}>
+            <Typography sx={getTextStyle(props)}>{props.message.text}</Typography>
+        </Grid>
+    </Grid>
+    )
 }
 
 const defultChat: IMessage[] = [
-    {text: "Hello", user: 1},
-    {text: "Hi", user: 2},
-    {text: "How are you?", user: 1},
-    {text: "I'm fine", user: 2},
-    {text: "What about you?", user: 2},
-    {text: "I'm also fine", user: 1},
-    {text: "Bye", user: 2},
-    {text: "Goodbye", user: 1},
+    {text: "Current topic is Music", user: 0, type: MessageType.ANNOUNCEMENT},
 ];
 
 export default function Page() {
   const [messages, setMessages] = useState<IMessage[]>(defultChat);
   const [message, setMessage] = useState<string>('');
-  const [user, setUser] = useState(1);
+  const [user, setUser] = useState(0);
+  const [open, setOpen] = useState(false);
 
   const getMessage = useCallback(() => {
     socket.on('message', (message: IMessage) => {
-      setMessages([...messages, message]);
+        console.log(messages);
+        setMessages([...messages, message]);
     });
   }, [messages]);
 
   const getUser = useCallback(() => {
     socket.on('newUser', (newUser: number) => {
-      console.log('newUser', newUser);
       if(user === 0) {
-        console.log("wewe");
         setUser(newUser);
       }
     });
@@ -134,7 +204,7 @@ export default function Page() {
             <InputAdornment position="end">
               <IconButton onClick={ () => {
                 if (message !== "") {
-                  socket.emit("message", {text: message, user}); 
+                  socket.emit("message", {text: message, user,type: MessageType.USER}); 
                   setMessage("");
                 }
               }}>
@@ -144,6 +214,33 @@ export default function Page() {
           }
         />
       </Grid>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+        <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+            Poll
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            <RadioGroup
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="female"
+                name="radio-buttons-group"
+            >
+                {pollOptions.map((option, index) => (
+                    <FormControlLabel key={index} value={option.value} control={<Radio />} label={option.label} />
+                ))}
+            </RadioGroup>
+            <Button variant="contained" sx={{ mt: 2 }} onClick={() => setOpen(false)}>
+                Let's go!
+            </Button>
+        </Box>
+        </Modal>
     </Stack>
   )
 }
